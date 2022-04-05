6a51b5a116b948e85f361d8f5f9a0e0bcf7d6868
Isaiah Eusebio <icce@hawaii.edu>
2022-04-04 18:50:15 -1000

    work on custom mazes

---
+++ The Backrooms/Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
+
+        if (playerInventory != null)
+        {
+            playerInventory.Collected();
+            gameObject.SetActive(false);
+        }
+    }
+}
